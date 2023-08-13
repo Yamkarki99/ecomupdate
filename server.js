7111ab409e4a8e637f88ff7c714e7b2b9e7b4c1e
yamkarki99 <99rajeevkarki@gmail.com>
2023-08-13 11:32:19 +1000

    crud for product done

---
+++ server.js
@@ -0,0 +1,56 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import express from "express";
+const app = express();
+const PORT = process.env.PORT || 8000;
+import path from "path";
+
+// middlewares
+import morgan from "morgan";
+import cors from "cors";
+import { mongoConnect } from "./src/config/mongoConfig.js";
+mongoConnect();
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+
+const __dirname = path.resolve();
+
+console.log(__dirname);
+//converting public folder to static serving folder
+app.use(express.static(path.join(__dirname, "/public")));
+
+// api
+import adminRouter from "./src/router/adminRouter.js";
+import categoryRouter from "./src/router/categoryRouter.js";
+import paymentOptionRouter from "./src/router/paymentOptionRouter.js";
+import { auth } from "./src/middleware/authMiddleware.js";
+import productRouter from "./src/router/productRouter.js";
+
+app.use("/api/v1/admin", adminRouter);
+app.use("/api/v1/category", auth, categoryRouter);
+app.use("/api/v1/payment-option", auth, paymentOptionRouter);
+app.use("/api/v1/product", auth, productRouter);
+
+app.get("/", (req, res) => {
+  res.json({
+    status: "success",
+    message: "Server is live",
+  });
+});
+
+app.use((error, req, res, next) => {
+  console.log(error);
+  const code = error.statusCode || 500;
+  res.status(code).json({
+    status: "error",
+    message: error.message,
+  });
+});
+
+app.listen(PORT, (error) => {
+  error
+    ? console.log(error)
+    : console.log(`server is running at http://localhost:8000`);
+});
---
+++ src/model/product/ProductModel.js
@@ -0,0 +1,30 @@
+import ProductSchema from "./ProductSchema.js";
+
+export const insertProduct = (obj) => {
+  return ProductSchema(obj).save();
+};
+
+export const getProducts = () => {
+  return ProductSchema.find();
+};
+
+export const getProductById = (_id) => {
+  return ProductSchema.findById(_id);
+};
+
+export const findOneProductByFilter = (filter) => {
+  return ProductSchema.findOne(filter);
+};
+
+export const updateProductById = ({ _id, ...rest }) => {
+  return ProductSchema.findByIdAndUpdate(_id, rest);
+};
+
+//@filter, @updateObj must be an obj
+export const updateproduct = (filter, updateObj) => {
+  return ProductSchema.findOneAndUpdate(filter, updateObj, { new: true });
+};
+
+export const deleteproductbyId = (_id) => {
+  return ProductSchema.findByIdAndDelete(_id);
+};
